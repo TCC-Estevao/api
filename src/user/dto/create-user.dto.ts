9825295fe017036effc29ba0ae6ea2ebdc975a7f
estevaoreis25 <estevaoreis1587@gmail.com>
2023-06-30 15:58:07 -0300

    chore: start validation fields

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,17 @@
-export class CreateUserDto {}
+import { IsEmail, IsString, Matches, MinLength } from 'class-validator';
+import { User } from '../entities/user.entity';
+
+export class CreateUserDto extends User {
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  @MinLength(4)
+  @Matches(/((?=.*\d)(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).{8,}$/, {
+    message: 'password too weak',
+  })
+  password: string;
+
+  @IsString()
+  name: string;
+}
